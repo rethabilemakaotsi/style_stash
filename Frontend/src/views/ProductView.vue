4ba76f637be3add2e95a2e2c007deb0538f82462
rethabile <rethabilemakaotsi17@gmail.com>
2024-03-13 14:43:21 +0200

    frontend

--- Frontend/src/views/ProductView.vue
+++ Frontend/src/views/ProductView.vue
@@ -3,39 +3,39 @@
         <div class="row">
             <h2 class="display-2">Product Details</h2>
         </div>
-        <div class="row">
-            <Card v-if="product">
-                <template #cardHeader>
-                    <img :src="product.imageURL" class="car-img-top" alt="" height="200">
-                </template>
-                <template #cardBody>
-                    <p class="card-text text-dark  p-2">
-                         {{ product.description }}
-                    </p>
-                    <p class="card-text text-dark bg-gradient bg-dark-subtle p-2">
-                        Amount: R{{ product.productPrice }}
-                    </p>
-                </template>
-            </Card>
+        <div class="row justify-content-center">
+            <div class="col-md-4">
+                <Card v-if="product">
+                    <template #cardHeader>
+                        <img :src="product.imageURL" class="card-img-top" alt="" height="200">
+                    </template>
+                </Card>
+            </div>
+            <div class="col-md-4">
+                <p class="card-text ">{{ product.description }}</p>
+                <button type="button" class="btn btn-lg py-2 text-white">LOGIN</button>
+            </div>
+            
         </div>
     </div>
 </template>
 
 <script>
 import Card from '@/components/Card.vue';
-    export default {
-        components:{
-            Card,
+
+export default {
+    components: {
+        Card,
+    },
+    computed: {
+        product() {
+            return this.$store.state.product;
         },
-        computed: {
-            product() {
-                return this.$store.state.product
-            }
-        },
-        mounted() {
-            this.$store.dispatch('fetchProduct', this.$route.params)
-        }
-    }
+    },
+    mounted() {
+        this.$store.dispatch('fetchProduct', this.$route.params);
+    },
+};
 </script>
 
 <style scoped>
