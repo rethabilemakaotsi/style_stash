ace96f94043ee8a125928849ecc224e77d25aaa0
rethabile <rethabilemakaotsi17@gmail.com>
2024-03-13 12:00:41 +0200

    frontend

--- Frontend/src/views/Products.vue
+++ Frontend/src/views/Products.vue
@@ -1,24 +1,26 @@
 <template>
-    <div>
-        
-    </div>
-    <div class="container ">
+    <div class="main">
+        <div class="container ">
         <div class="row ">
             <div class="col">
-                <input type="text" placeholder="Search product by name" class="form-control">
+                <button class="btn ">Sorting by price</button>
             </div>
             <div class="col">
-                <button class="btn btn-success">Sorting by price</button>
+                <input type="text" placeholder="Search product by name" class="form-control" >
+            </div>
+            <div class="col">
+                <button class="btn ">Sorting by price</button>
             </div>
         </div>
-        <div class="row mx-5 " v-if="products">
-            <Card v-for="product in products" :key="product.productID" class="mb-3">
+        <div class="row mx-5 gap-5 justify-content-center  " v-if="products">
+            <Card v-for="product in products" :key="product.productID" class="mb-3 ">
                 <template #cardHeader>
+                    
                     <img :src="product.imageURL" class="car-img-top" alt="" height="200">
                     <h4 class="card-title">{{ product.productName }}</h4>
                 </template>
                 <template #cardBody>
-                    <p class="card-text text-dark bg-gradient bg-dark-subtle p-2">
+                    <p class="card-text text-dark  p-2">
                         Amount: R{{ product.productPrice }}
                     </p>
                     <router-link :to="{name: 'products', params: {id: product.productID}}">View More</router-link>
@@ -29,6 +31,9 @@
             <p class="lead">Loading</p>
         </div>
     </div>
+        
+    </div>
+    
 </template>
 
 <script>
@@ -50,7 +55,46 @@ export default {
 </script>
 
 <style scoped>
+.form-control{
+    width: 290px;
+    margin-left: 100px;
 
+    
+}
+.btn {
+  border: 2px solid #af8c53;
+  color: #af8c53;
+  padding:  10px;
+  background-color: transparent;
+  margin-left: 150px;
+
+ 
+
+}
+.main{
+    height: 200vh;
+    background-color:#252926 ;
+}
+.mb-3 {
+  border: 2px solid #af8c53;
+  border-radius: 10px;
+  background-color: #fff;
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+}
+
+.card-title,
+.card-text {
+  text-transform: uppercase;
+  font-family: 'DM sans';
+  font-size: medium;
+  color: #af8c53;
+}
+
+.card:hover {
+  transform: translateY(-5px);
+  box-shadow: 0px 8px 16px rgba(0, 0, 0, 0.2);
+  transition: transform 0.3s ease-in-out, box-shadow 0.3s ease-in-out;
+}
 
 
 
