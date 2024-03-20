d1a842a1fa38cd2104ea1b4c264f2d95244bd9d5
rethabile <rethabilemakaotsi17@gmail.com>
2024-03-20 09:32:14 +0200

    frontend

--- Frontend/src/views/CheckoutView.vue
+++ Frontend/src/views/CheckoutView.vue
@@ -1,76 +1,168 @@
 <template>
-  <div class="cart">
-    <h1 class="cart-heading">Your Cart</h1>
-    <router-link class="nav-link" to="/products">
-      <button class="back-button">Add Other Product</button>
-    </router-link>
-    <table v-if="cartItems && cartItems.length > 0">
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>NAME</th>
-          <th>IMAGE</th>
-          <th>QUANTITY</th>
-          <th>Amount</th>
-          <th>Action</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(product, index) in cartItems" :key="index">
-          <td>{{ product.productID }}</td>
-          <td>{{ product.productName }}</td>
-          <td><img :src="product.productImgURL" :alt="product.productImgURL" /></td>
-          <td>{{ product.productQuantity }}</td>
-          <td>R{{ formatAmount(product.productAmount) }}</td>
-          <td>
-            <button @click="removeItem(index)">
-              <i class="bi bi-trash"></i>
-            </button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-    <p v-else class="empty-cart-text">Nothing in your cart</p>
-    <p class="subtotal">Subtotal: R{{ calculateSubtotal().toFixed(1) }}</p>
-    <button class="pay-button" @click="order">Order</button>
-  </div>
+  <body>
+      <div class="container">
+          <div class="row">
+              <h2 class="display-4">
+                  <button class="btn btn-primary mr-2" @click="showUsers = true; showProducts = false"
+                      style="background-color: #4CAF50; color: white; padding: 10px 20px; border: none; border-radius: 5px; cursor: pointer;">
+                      Users
+                  </button>
+                  <button class="btn btn-primary mr-2" @click="showProducts = true; showUsers = false"
+                      style="background-color: #4CAF50; color: white; padding: 10px 20px; border: none; border-radius: 5px; cursor: pointer;">
+                      Products
+                  </button>
+              </h2>
+          </div>
+          <div class="row" v-show="showUsers && users">
+              <!-- Modal -->
+              <div class="btn-tdn mb-2 ">
+                  <button class="btn btn-success edt-btn mr-6" data-bs-toggle="modal" data-bs-target="#add">add
+                      User</button>
+              </div>
+              <div class="modal fade" id="add" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                  <div class="modal-dialog">
+                      <div class="modal-content">
+                          <div class="modal-header">
+                              <h1 class="modal-title fs-5" id="add">Add Users</h1>
+                              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                          </div>
+                          <div class="modal-body">
+                              <input class="w-100" type="text" placeholder=" First Name">
+                              <input class="w-100" type="text" placeholder=" Last Name">
+                              <input class="w-100" type="text" placeholder=" Age">
+                              <input class="w-100" type="text" placeholder="gender">
+                              <input class="w-100" type="text" placeholder="Email">
+                              <input class="w-100" type="text" placeholder="Password">
+                              <input class="w-100" type="text" placeholder="Role">
+                          </div>
+                          <div class="modal-footer">
+                              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                              <button type="button" class="btn btn-primary">Save changes</button>
+                          </div>
+                      </div>
+
+                  </div>
+              </div>
+              <table class="table table-bordered">
+                  <thead>
+                      <tr>
+                          <th>User ID</th>
+                          <th>First name</th>
+                          <th>Last name</th>
+                          <th>User age</th>
+                          <th>Gender</th>
+                          <th>Email address</th>
+                          <th>User role</th>
+                          <th>Action</th>
+                      </tr>
+                  </thead>
+                  <tbody>
+                      <tr v-for="user in users" :key="user.userID">
+                          <td>{{ user.userID }}</td>
+                          <td>{{ user.firstName }}</td>
+                          <td>{{ user.lastName }}</td>
+                    
+                          <td>{{ user.email }}</td>
+                          <td>{{ user.userRole }}</td>
+                          <td class="d-flex justify-content-between">
+                              <button class="btn btn-success">Edit</button>
+                              <button class="btn btn-success">Delete</button>
+                          </td>
+                      </tr>
+                  </tbody>
+              </table>
+          </div>
+          <div class="row" v-show="showProducts && products">
+              <!-- Modal -->
+              <div class="btn-tdn mb-2 ">
+                  <button class="btn btn-success edt-btn mr-6" data-bs-toggle="modal" data-bs-target="#addProductModal">Add
+                      Product</button>
+              </div>
+              <div class="modal fade" id="addProductModal" tabindex="-1" aria-labelledby="addProductModalLabel" aria-hidden="true">
+                  <div class="modal-dialog">
+                      <div class="modal-content">
+                          <div class="modal-header">
+                              <h1 class="modal-title fs-5" id="add">Add Product</h1>
+                              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                          </div>
+                          <div class="modal-body">
+                              <input class="w-100" type="text" placeholder=" First Name">
+                              <input class="w-100" type="text" placeholder=" Last Name">
+                              <input class="w-100" type="text" placeholder=" Age">
+                              <input class="w-100" type="text" placeholder="gender">
+                              <input class="w-100" type="text" placeholder="Email">
+                              <input class="w-100" type="text" placeholder="Password">
+                              <input class="w-100" type="text" placeholder="Role">
+                          </div>
+                          <div class="modal-footer">
+                              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                              <button type="button" class="btn btn-primary">Save changes</button>
+                          </div>
+                      </div>
+
+                  </div>
+              </div>
+
+              <table class="table table-bordered">
+                  <thead>
+                      <tr>
+                          <th>Product ID</th>
+                          <th>Product Name</th>
+                          <th>Product Quantity</th>
+                          <th>Product Amount</th>
+                          <th>Action</th>
+                      </tr>
+                  </thead>
+                  <tbody>
+                      <tr v-for="product in products" :key="product.productID">
+                          <td>{{ product.productID }}</td>
+                          <td>{{ product.productName }}</td>
+                          <td>{{ product.productPrice }}</td>
+                          <td>R {{ product.description }}</td>
+                          <td class="d-flex justify-content-between">
+                              <button class="btn btn-success">Edit</button>
+                              <button class="btn btn-success">Delete</button>
+                          </td>
+                      </tr>
+                  </tbody>
+              </table>
+
+          </div>
+      </div>
+  </body>
 </template>
-
 <script>
-import { computed } from 'vue';
-import { useStore } from 'vuex';
-
 export default {
-  setup() {
-    const store = useStore();
-
-    const cartItems = computed(() => store.getters.cartItems);
-
-    const removeItem = (index) => {
-      store.commit('removeFromCart', index);
-    };
-
-    const calculateSubtotal = () => {
-      if (!cartItems.value) return 0;
-
-      return cartItems.value.reduce(
-        (total, product) => total + (product.productAmount * product.productQuantity),
-        0
-      );
-    };
-
-    const formatAmount = (amount) => {
-      return parseFloat(amount).toFixed(1);
-    };
-
-   
-    return {
-      cartItems,
-      removeItem,
-      calculateSubtotal,
-      formatAmount,
-   
-    };
+  data() {
+      return {
+          showUsers: true,
+          showProducts: false,
+      };
+  },
+  computed: {
+      users() {
+          return this.$store.state.users;
+      },
+      products() {
+          return this.$store.state.products;
+      },
+  },
+  mounted() {
+      this.$store.dispatch("fetchUsers");
+      this.$store.dispatch("fetchProducts");
   },
 };
 </script>
+<style scoped>
+body {
+  background-color: #918E8E;
+}
+
+.edt-btn {
+  position: relative;
+}
+
+.btn-tdn {
+  width: 30%;
+}
+</style>
