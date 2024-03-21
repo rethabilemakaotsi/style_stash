f66d6065fce12fa5d2122d51532addc058ff1eed
rethabile <rethabilemakaotsi17@gmail.com>
2024-03-21 14:33:14 +0200

    backend

--- index.js
+++ index.js
@@ -11,16 +11,26 @@ config()
 const app = express()
 const port = +process.env.PORT ||4000
 
-app.use((req, res, next) =>{
-  res.header("Access-control-Allow-Origin", "*");
-  res.header("Access-control-Allow-Credentials", "true");
-  res.header("Access-control-Allow-Methods", "*");
-  res.header("Access-control-Request-Methods", "*");
-  res.header("Access-control-Allow-Headers", "*");
-  res.header("Access-control-Expose-Headers", "Authorization");
-  next()
+// app.use((req, res, next) =>{
+//   res.header("Access-control-Allow-Origin", "*");
+//   res.header("Access-control-Allow-Credentials", "true");
+//   res.header("Access-control-Allow-Methods", "*");
+//   res.header("Access-control-Request-Methods", "*");
+//   res.header("Access-control-Allow-Headers", "*");
+//   res.header("Access-control-Expose-Headers", "Authorization");
+//   next()
 
-})
+// })
+// app.use(
+//     express.static('./static'),
+//     express.json(),
+//     express.urlencoded({
+//         extended: true,
+//     }),
+//     cookieParser(),
+//     cors()
+// )
+
 app.use(
     express.static('./static'),
     express.json(),
@@ -28,8 +38,10 @@ app.use(
         extended: true,
     }),
     cookieParser(),
-    cors()
-)
+    cors({
+        origin: '*'
+    })
+);
 app.get('^/$|/style_stash', (req, res) => {
     res.status(200).sendfile(path.join(__dirname, './static/index.html'))
 })
